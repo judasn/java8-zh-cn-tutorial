1f648224361ef8fd1ccb84ede7c03a1b5da3ae1d
judasn <jn3.141592654@gmail.com>
2018-06-27 23:05:22 +0800

    2018-06-27 补充接口新特性

---
+++ src/test/java/com/youmeek/java/sample/main/AnonymousInnerClassTest.java
@@ -0,0 +1,96 @@
+package com.youmeek.java.sample.main;
+
+import org.junit.Test;
+
+import java.text.Collator;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * lambda 表达式（匿名函数）在简单使用上就是用编写函数的方式编写匿名内部类代码
+ * 所以学习 lambda 一定要多写写匿名内部类，写多了会觉得那个结构很吐血，才能体会 lambda
+ */
+public class AnonymousInnerClassTest {
+
+	//=====================================实现 Runnable start=====================================
+
+	@Test
+	public void test1() {
+		new Runnable() {
+			@Override
+			public void run() {
+				System.out.println("--------------------------------匿名内部类实现 Runnable 接口");
+			}
+		}.run();
+	}
+
+	/**
+	 * 写法 1
+	 */
+	@Test
+	public void test11() {
+		Runnable r = () -> {
+			System.out.println("--------------------------------");
+		};
+		r.run();
+	}
+
+	/**
+	 * 写法 2
+	 */
+	@Test
+	public void test111() {
+		((Runnable) () -> System.out.println("--------------------------------匿名内部类实现 Runnable 接口")).run();
+	}
+
+	//=====================================实现 Runnable  end=====================================
+
+	//=====================================Collections sort start=====================================
+
+	@Test
+	public void test2() {
+		List<String> list = Arrays.asList("a", "b", "c");
+		Collections.sort(list, new Comparator<String>() {
+			@Override
+			public int compare(String o1, String o2) {
+				return Collator.getInstance().compare(o1, o2);
+			}
+		});
+	}
+
+	/**
+	 * 写法 1
+	 */
+	@Test
+	public void test22() {
+
+		List<String> list = Arrays.asList("a", "b", "c");
+		Collections.sort(list, (String o1, String o2) -> {
+			return Collator.getInstance().compare(o1, o2);
+		});
+	}
+
+	/**
+	 * 写法 2
+	 */
+	@Test
+	public void test222() {
+		List<String> list = Arrays.asList("a", "b", "c");
+		Collections.sort(list, (String o1, String o2) -> Collator.getInstance().compare(o1, o2));
+	}
+
+	/**
+	 * 写法 3
+	 */
+	@Test
+	public void test2222() {
+		List<String> list = Arrays.asList("a", "b", "c");
+		Collections.sort(list, (o1, o2) -> Collator.getInstance().compare(o1, o2));
+	}
+
+	//=====================================Collections sort  end=====================================
+
+}
+
